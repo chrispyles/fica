d238c9c27db04d2e0c5437b72d97efbe861ef813
Chris Pyles <cpyles@berkeley.edu>
2023-08-08 18:12:08 -0700

    add logo and switch to furo theme due to pydata-sphinx-theme logo error

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath('.'))
 # -- Project information -----------------------------------------------------
 
 project = 'fica'
-copyright = '2022, Christopher Pyles'
+copyright = '2022-23, Christopher Pyles'
 author = 'Christopher Pyles'
 
 
@@ -48,9 +48,11 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'pydata_sphinx_theme'
+html_theme = 'furo'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_logo = '../images/logo.svg'
